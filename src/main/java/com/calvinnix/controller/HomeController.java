a2836962a2ba3ebfa48ef33d6925b2f1b16595d1
Calvin Nix <calvinnix28@gmail.com>
2017-01-09 11:02:58 -0500

    initial commit

---
+++ src/main/java/com/calvinnix/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.calvinnix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Calvin on 1/9/17.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+}
