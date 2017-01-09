9e776802207000884f63c5d3aee4d2438e80b92a
Calvin Nix <calvinnix28@gmail.com>
2017-01-09 13:29:53 -0500

    Finished boiler plate setup to pass data via Java backend

---
+++ src/main/java/com/calvinnix/dao/EmployeeDao.java
@@ -0,0 +1,13 @@
+package com.calvinnix.dao;
+
+import com.calvinnix.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by Calvin on 1/9/17.
+ */
+
+@Repository
+public interface EmployeeDao extends CrudRepository<Employee, Long> {
+}
--- src/main/java/com/calvinnix/model/Employee.java
+++ src/main/java/com/calvinnix/model/Employee.java
@@ -1,7 +1,30 @@
 package com.calvinnix.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by Calvin on 1/9/17.
  */
+
+
+@Entity
 public class Employee {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+    private int years;
+
+    private Employee() {}
+
+    public Employee(String name, int age, int years) {
+        this.name = name;
+        this.age = age;
+        this.years = years;
+    }
 }
