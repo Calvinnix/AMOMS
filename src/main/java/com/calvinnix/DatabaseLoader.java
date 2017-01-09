363be6fb5e66edbc07af5cbc1a5a572d25bb8f39
Calvin Nix <calvinnix28@gmail.com>
2017-01-09 13:38:54 -0500

    Created CommandLineRunner to load backend database

---
+++ src/main/java/com/calvinnix/DatabaseLoader.java
@@ -0,0 +1,30 @@
+package com.calvinnix;
+
+import com.calvinnix.dao.EmployeeDao;
+import com.calvinnix.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Calvin on 1/9/17.
+ */
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeDao employeeDao;
+
+    @Autowired
+    public DatabaseLoader(EmployeeDao employeeDao) {
+        this.employeeDao = employeeDao;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.employeeDao.save(new Employee("Calvin Nix", 22, 3));
+        this.employeeDao.save(new Employee("Alex Estrada", 23, 2));
+        this.employeeDao.save(new Employee("Eric Plascencia", 23, 2));
+        this.employeeDao.save(new Employee("Zacch Thomas", 46, 5));
+    }
+}
