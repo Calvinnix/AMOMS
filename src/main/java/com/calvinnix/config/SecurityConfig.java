d7f9eca9c6b98c6d9fd18f227559d21674e94614
Calvin Nix <calvinnix28@gmail.com>
2017-01-10 09:19:10 -0500

    Modified SecurityConfig to remove stormpath

--- src/main/java/com/calvinnix/config/SecurityConfig.java
+++ src/main/java/com/calvinnix/config/SecurityConfig.java
@@ -1,14 +1,8 @@
 package com.calvinnix.config;
 
-import com.stormpath.sdk.account.Account;
-import com.stormpath.sdk.application.Application;
-import com.stormpath.sdk.client.Client;
-import com.stormpath.sdk.client.Clients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import static com.stormpath.spring.config.StormpathWebSecurityConfigurer.stormpath;
 
 /**
  * Created by Calvin on 1/9/17.
@@ -19,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.apply(stormpath());
+
     }
 
 
