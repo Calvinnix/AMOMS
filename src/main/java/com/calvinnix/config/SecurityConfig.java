267a4de9198cb3157c3b3b165558c166eef0bce0
Calvin Nix <calvinnix28@gmail.com>
2017-01-10 09:29:14 -0500

    There were some additional references to spring security I had to remove.

--- src/main/java/com/calvinnix/config/SecurityConfig.java
+++ src/main/java/com/calvinnix/config/SecurityConfig.java
@@ -1,20 +1,13 @@
 package com.calvinnix.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 /**
  * Created by Calvin on 1/9/17.
  */
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-    }
 
 
 
--- src/main/java/com/calvinnix/dao/EmployeeDao.java
+++ src/main/java/com/calvinnix/dao/EmployeeDao.java
@@ -2,7 +2,6 @@ package com.calvinnix.dao;
 
 import com.calvinnix.model.Employee;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EmployeeDao extends CrudRepository<Employee, Long> {
 
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Override
     void delete(Long aLong);
 
