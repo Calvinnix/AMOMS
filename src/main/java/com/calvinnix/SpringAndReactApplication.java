76342d45526a54256717ad36ec54ab7346851722
Calvin Nix <calvinnix28@gmail.com>
2017-01-11 18:48:47 -0500

    Added TODOs I need to finish to complete the app

--- src/main/java/com/calvinnix/SpringAndReactApplication.java
+++ src/main/java/com/calvinnix/SpringAndReactApplication.java
@@ -6,6 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAndReactApplication {
 
+	//TODO:ctn add ability to add/remove employees (admin only)
+	//TODO:ctn add unit tests
+	//TODO:ctn use webpack to load resources
+	//TODO:ctn use scss instead of css
+	//TODO:ctn add remember me functionality
+	//TODO:ctn add sign up functionality
+	//TODO:ctn fix react signup bug
+	//TODO:ctn sanitize inputs
+	//TODO:ctn escape outputs
+	//TODO:ctn re-enable csrf
+	//TODO:ctn Refactor code
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAndReactApplication.class, args);
 	}
