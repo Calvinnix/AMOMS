5d454852bb74b793ae281b9546941c6e7d0eb984
Calvinnix <calvinnix28@gmail.com>
2017-01-16 19:48:06 -0500

    Added frontend validation for login and signup form

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -90,3 +90,129 @@ var App = React.createClass({
 if (document.getElementById('root') != null) {
     ReactDOM.render(<App />, document.getElementById('root'));
 }
+
+
+
+/*TODO:ctn Eventually will want to convert this code (as well as the login/signup page) to utilize REACT */
+/*TODO:ctn some code is repeated... This should be cleaned up */
+
+$("#btnLogin").click(function(e) {
+  validateLoginForm($(this), e);
+});
+
+$("#btnSignup").click(function(e) {
+  validateSignupForm($(this), e);
+});
+
+function validateLoginForm(element, e) {
+    var username = $("#inputUsername");
+    var password = $("#inputPassword");
+    var errorMessage = "";
+
+    element.siblings("div.errorDiv").each(function() {
+      $(this).remove();
+    });
+
+    var usernameErrorMessage = returnUsernameErrorMessage(username);
+    if ( usernameErrorMessage.length !== 0 ) {
+      errorMessage += usernameErrorMessage;
+      username.addClass("invalid");
+      username.removeClass("valid");
+    } else {
+      username.addClass("valid");
+      username.removeClass("invalid");
+    }
+
+    var passwordErrorMessage = returnPasswordErrorMessage(password);
+    if ( passwordErrorMessage.length !== 0 ) {
+      errorMessage += passwordErrorMessage;
+      password.addClass("invalid");
+      password.removeClass("valid");
+    } else {
+      password.addClass("valid");
+      password.removeClass("invalid");
+    }
+
+    if (errorMessage.length !== 0) {
+      e.preventDefault();
+      element.parent().prepend('<div class="errorDiv"><div class="alert alert-danger">'+errorMessage+'</div></div>');
+    }
+}
+
+function validateSignupForm(element, e) {
+    var username = $("#inputUsername");
+    var password = $("#inputPassword");
+    var confirmPassword = $("#inputConfirmPassword")
+    var errorMessage = "";
+
+    element.siblings("div.errorDiv").each(function() {
+      $(this).remove();
+    });
+
+    var usernameErrorMessage = returnUsernameErrorMessage(username);
+    if ( usernameErrorMessage.length !== 0 ) {
+      errorMessage += usernameErrorMessage;
+      username.addClass("invalid");
+      username.removeClass("valid");
+    } else {
+      username.addClass("valid");
+      username.removeClass("invalid");
+    }
+
+    var passwordErrorMessage = returnPasswordErrorMessage(password);
+    if ( passwordErrorMessage.length !== 0 ) {
+      errorMessage += passwordErrorMessage;
+      password.addClass("invalid");
+      password.removeClass("valid");
+    } else {
+      password.addClass("valid");
+      password.removeClass("invalid");
+    }
+
+    var confirmPasswordErrorMessage = returnConfirmPasswordErrorMessage(password, confirmPassword);
+    if ( confirmPasswordErrorMessage.length !== 0 ) {
+      errorMessage += confirmPasswordErrorMessage;
+      confirmPassword.addClass("invalid");
+      confirmPassword.removeClass("valid");
+    } else {
+      confirmPassword.addClass("valid");
+      confirmPassword.removeClass("invalid");
+    }
+
+    if (errorMessage.length !== 0) {
+      e.preventDefault();
+      element.parent().prepend('<div class="errorDiv"><div class="alert alert-danger">'+errorMessage+'</div></div>');
+    }
+}
+
+
+
+function returnUsernameErrorMessage(username) {
+  var errorMessage = "";
+  if (username.val().length < 4) {
+    errorMessage += "Username must be 4 or more characters.\n";
+  }
+  if (username.val().length > 20) {
+    errorMessage += "Username must be less than 20 characters.\n";
+  }
+  return errorMessage;
+}
+
+function returnPasswordErrorMessage(password) {
+    var errorMessage = "";
+    if (password.val().length < 8) {
+      errorMessage += "Password must be 8 or more characters.\n";
+    }
+    if (password.val().length > 32) {
+      errorMessage += "Password must be less than 32 characters.\n";
+    }
+  return errorMessage;
+}
+
+function returnConfirmPasswordErrorMessage(password, confirmPassword) {
+  var errorMessage = "";
+  if (password.val() !== confirmPassword.val()) {
+    errorMessage += "The confirmation password does not match.\n";
+  }
+  return errorMessage;
+}
