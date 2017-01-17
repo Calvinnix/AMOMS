7ede5e64484acf58c73abea01088a010efd4d94d
Calvin Nix <calvinnix28@gmail.com>
2017-01-17 15:48:28 -0500

    Finished checking off TODO for adding tests

--- src/main/java/com/calvinnix/SpringAndReactApplication.java
+++ src/main/java/com/calvinnix/SpringAndReactApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAndReactApplication {
 
-	//TODO:ctn add unit tests
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAndReactApplication.class, args);
 	}
