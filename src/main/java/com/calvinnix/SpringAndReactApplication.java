1038ddb24ed7fbc1fc1a40321db1353b605ed740
Calvinnix <calvinnix28@gmail.com>
2017-01-16 21:00:55 -0500

    Marked re-enable CSRF todo as finished

--- src/main/java/com/calvinnix/SpringAndReactApplication.java
+++ src/main/java/com/calvinnix/SpringAndReactApplication.java
@@ -9,7 +9,6 @@ public class SpringAndReactApplication {
 	//TODO:ctn add unit tests
 	//TODO:ctn better way to load resources?
 	//TODO:ctn security (sql injection)
-	//TODO:ctn re-enable csrf
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAndReactApplication.class, args);
