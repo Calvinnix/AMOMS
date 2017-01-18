004de6f0077516d4e154e6ae3b1423531e3e2f89
Calvin Nix <calvinnix28@gmail.com>
2017-01-17 19:02:21 -0500

    Renamed old project name to Amoms

--- src/main/java/com/calvinnix/Amoms.java
+++ src/main/java/com/calvinnix/Amoms.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAndReactApplication {
+public class Amoms {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAndReactApplication.class, args);
+		SpringApplication.run(Amoms.class, args);
 	}
 }
