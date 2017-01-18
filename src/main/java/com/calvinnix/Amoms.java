8092f7e959574b2d3cc1af93a693ffea0eeaf557
Calvinnix <calvinnix28@gmail.com>
2017-01-17 20:42:59 -0500

    Updated CommandReferences.txt with some more how-to information

--- src/main/java/com/calvinnix/Amoms.java
+++ src/main/java/com/calvinnix/Amoms.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Amoms {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Amoms.class, args);
 	}
